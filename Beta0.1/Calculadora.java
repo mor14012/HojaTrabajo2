c9dc2c1b8ab84c5cd28a127d749046c42e780441
Link-djst <Link-djst@users.noreply.github.com>
2015-07-28 14:28:20 -0600

    Update Calculadora.java

--- Beta0.1/Calculadora.java
+++ Beta0.1/Calculadora.java
@@ -9,15 +9,127 @@ Julio González. Carné: 14096
 Hoja de Trabajo 2
 */
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 
 public class Calculadora<P> implements ADTCalculadora<P>{
+	private String lineWspace;
 	private P value;
-	public void readFile(String file) throws FileNotFoundException{
+	private Double a,b,c;
+	private int counter;
+	private boolean first;
+	
+	Stack<Double> nstack = new Stack<Double>(10);
+	public void readFile(String file) throws FileNotFoundException, Exception{
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+		    String line;
+		    while ((line = br.readLine()) != null) {
+		       lineWspace = line.replaceAll(" ", "");
+		       System.out.println("Resultado: "+calcular());
+		       //System.out.println(lineWspace);
+		    }
+		}
 		System.out.println("readFile");
+				
 	}
-	public P calcular(){
+	public P calcular() throws Exception{
+		first = true;
+		
+		for (int i = 0;i < lineWspace.length(); i++){
+	    	//System.out.println(lineWspace.charAt(i));
+		    if (lineWspace.charAt(i) == 43){
+		    	counter = 0;
+				try {
+					a = nstack.pop();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		    	
+				try {
+					b = nstack.pop();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		    	
+				c = a+b;
+		    	nstack.push(c);
+		    	
+		    	//System.out.println("Encontramos una suma en "+i);
+		    } else if (lineWspace.charAt(i) == 42){
+		    	counter = 0;
+		    	try {
+					a = nstack.pop();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		    	
+				try {
+					b = nstack.pop();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		    	
+				c = a*b;
+		    	nstack.push(c);
+		    	
+		    	//System.out.println("Encontramos una multiplicación en "+i);
+		    } else if (lineWspace.charAt(i) == 45) {
+		    	counter = 0;
+		    	try {
+					a = nstack.pop();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		    	
+				try {
+					b = nstack.pop();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		    	
+				c = a-b;
+		    	nstack.push(c);
+		    	
+		    	//System.out.println("Encontramos una resta en "+i);
+		    } else if (lineWspace.charAt(i) == 47){
+		    	try {
+					a = nstack.pop();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		    	
+				try {
+					b = nstack.pop();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		    	
+				c = a/b;
+		    	nstack.push(c);
+		    	
+		    	//System.out.println("Encontramos una división en "+i);	
+		    } else {
+		    	nstack.push((double)Character.getNumericValue(lineWspace.charAt(i)));
+		    }
+		}
 		System.out.println("calcular");
+		
+		try {
+			value = (P) nstack.pop();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return value;
 	}
 	
